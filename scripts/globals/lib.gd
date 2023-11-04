72da163f3760c5f5f052c7b65d643eb651a5b0ba
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2023-11-04 03:12:25 -0400

    e

--- scripts/globals/lib.gd
+++ scripts/globals/lib.gd
@@ -17,20 +17,6 @@ func rand_chance(chance: float):
 
 func time(): return Time.get_ticks_msec() / 1000.0
 
-func wait_for_seconds(length: float):
-	await get_tree().create_timer(length).timeout
-
-func transition(length: float, callback: Callable, backwards: bool = false):
-	var start = time()
-	
-	while time() - start < length:
-		var val = clampf((time() - start) / length, 0.0, 1.0)
-		callback.call(val if not backwards else 1.0 - val)
-		
-		await get_tree().process_frame
-	
-	callback.call(1.0 if not backwards else 0.0)
-
 func debounce(length: float, action: Callable) -> Callable:
 	var _debouncer := {
 		timer = null
@@ -85,81 +71,9 @@ func clear_child_nodes(parent_node: Node) -> void:
 	for node in parent_node.get_children():
 		node.queue_free()
 
-# Regex functions
-func regex_get_matches(pattern: String, search: String) -> Array:
-	var regex = RegEx.new()
-	regex.compile(pattern)
-	
-	return regex.search_all(search)
-
-func regex_get_matches_str(pattern: String, search: String):
-	var output = []
-	
-	for m in regex_get_matches(pattern, search):
-		output.append(m.get_string())
-	
-	return output
-
-# Other functions
-func pascal_case_to_spaces(input: String) -> String:
-	return " ".join(regex_get_matches_str("[A-Z][^A-Z]*", input))
-
-func get_prop(dictionary: Dictionary, path: Array) -> Variant:
-	var walk = dictionary
-	
-	for entry in path:
-		if entry == path[-1]: return walk[entry]
-		if entry not in walk: return null
-		
-		walk = walk[entry]
-	return null
-
-func set_prop(dictionary: Dictionary, path: Array, new_value: Variant) -> Dictionary:
-	var walk = dictionary
-	
-	for entry in path:
-		if typeof(entry) == typeof(path[-1]) and entry == path[-1]:
-			walk[entry] = new_value
-			return dictionary
-		
-		if entry not in walk: walk[entry] = {}
-		walk = walk[entry]
-	return dictionary
-
-func play_audio_at_point(point: Vector3, audio: Variant, volume_range: Array[float] = [50, 50],\
-						pitch_range: Array[float] = [1, 1], max_distance: float = 20) -> void:
-	if audio is String: audio = load(audio)
-	var player = AudioStreamPlayer3D.new()
-	
-	player.stream = audio
-	player.max_distance = max_distance
-	player.pitch_scale = randf_range(pitch_range[0], pitch_range[1])
-	player.volume_db = randf_range(volume_range[0], volume_range[1])
-	player.finished.connect(player.queue_free)
-	
-	get_tree().get_root().add_child(player)
-	player.global_transform.origin = point
-	player.play()
-
-func extend(input: Variant, properties: Dictionary) -> Variant:
-	for prop_name in properties:
-		if not prop_name in input: continue
-		
-		input[prop_name] = properties[prop_name]
-	return input
-
 func create_item_instance(node):
 	var new_node = node.duplicate()
 	new_node.show()
 	
 	node.queue_free()
 	return new_node
-
-func add_timer(target: Node, wait_time: float, callback: Callable) -> Timer:
-	var timer := Timer.new()
-	timer.wait_time = wait_time
-	timer.autostart = true
-	timer.timeout.connect(callback)
-	target.add_child(timer)
-	
-	return timer
